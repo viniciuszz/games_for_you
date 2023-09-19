61ea7c069e24b480f128825d1b62115874dffc72
vinicius <129230299+viniciuszz@users.noreply.github.com>
2023-09-19 17:36:23 -0300

    Add files via upload

---
+++ html/index.html.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icons/G4u.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" /> <!-- Link para os icones dos slides -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> <!-- Link para os icones do google icons, usados no footer e no menu -->
+    <link href="https://fonts.googleapis.com/css2?family=Koulen&family=PT+Sans&display=swap" rel="stylesheet"> <!-- Link para as fontes do Slide -->
+    <link rel="stylesheet" href="../css/slide.css">
+    <link rel="stylesheet" href="../css/pagprodutos.css">
+    <link rel="stylesheet" href="../css/EstiloMenu.css">
+    <link rel="stylesheet" href="../css/rodape.css">
+    <title>Games For You</title>
+</head>
+<body onload="carregar()">
+
+<!--Inicio do cabeçalho do site/menu -->
+<header id="header" class="gradient col-lg-12 col-md-12 col-sm-12">
+
+    <div class="cxlogo col-lg-1 col-md-1 col-sm-1">
+        <!-- redireciona para a index do nosso site -->
+        <a href="index.php">
+            <!-- imagem da logo do nosso site -->
+            <img class="logo" src="icons/G4u.png">
+        </a>
+    </div>
+    <!-- Barra de pesquisa -->
+    <div class="barra-pesquisa col-lg-9 col-md-9 col-sm-9">
+        <!-- formulario que envia  as informações da pesquisa  -->
+        <form method="get" action="pesquisa.php">
+            <!-- icone de lupa que fica na barra de pesquisa  -->
+            <span class="icon-search material-symbols-outlined">
+                search
+            </span>
+            <!-- campo para realizar a pesquisa do produto -->
+            <input class="procurar" type="search" name="pesquisa" placeholder="Pesquisar...">
+        </form>
+    </div>
+
+    <nav id="nav" class="col-lg-9 col-md-9 col-sm-9">
+        <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+            <span id="hamburger"></span>
+        </button>
+        <ul id="menu" role="menu">
+            <!-- redirecionamento das paginas do menu  -->
+            <li><a href="index.php">Home</a></li>
+            <li><a href="pagxbox.php">Xbox</a></li>
+            <li><a href="pagplaystation.php">Playstation</a></li>
+            <li><a class="borda-nintendo" href="pagnintendo.php">Nintendo</a></li>
+        </ul>
+    </nav>
+    </div>
+<!-- div para a exibisão do icone de usuario e nome do usuario -->
+    <div class="cxlogin col-sm-4 col-md-2 col-lg-1">
+    <div  class="carregamento"></div>
+    <div  class="carregado">
+        <?php
+        //inicia a session
+        session_start();
+        //caso não exista session de usuario ele não ira mostrar o nome  o icone de usuario irá o redirecionar para o login cadastro 
+        if (!isset($_SESSION['usuario'])) {
+            echo "";
+        ?>
+         <!-- redireciona o usuario para fazer o cadastro e o login -->
+            <a href="cadastro_login.php">
+                <button type="image" class="btn"><span class="material-symbols-outlined login">
+                        account_circle
+                    </span></button>
+            </a>
+        <?php
+        //mostra o nome e o icone de usuario redireciona para o historico de compras
+        } else {
+        ?>
+        <!-- redireciona o usuario para o seu historico de compras -->
+            <a href="historico.php">
+                <!-- botão que mostra o icone de usuario -->
+                <button type="image" class="btn"><span class="material-symbols-outlined login">
+                        account_circle
+                    </span></button>
+            </a>
+        <?php
+        //div para mudar a cor do nome do usuario 
+            echo  "<div class='eee'>";
+            //mostra o nome do usuario 
+            echo $_SESSION['usuario'];
+            echo "<a class='sair' href='../php/sair.php'>sair</a>";
+            echo "</div>";
+        }
+        ?>
+        </div>
+    </div>
+    <div class="col-sm-1 col-md-1 col-lg-1">
+    <div  class="carregamento1"></div>
+    <div  class="carregado1">
+        <a href="carrinho.php">
+            <button type="image" class="btn"><span class="material-symbols-outlined login">
+                    shopping_cart
+                </span></button>
+            <div class="numero-carrinho">
+                <?php
+                //mostra quantos produtos tem no carrinho de compras se não exitir nenhum item ele exibe o numero 0
+                if (!isset($_SESSION['itens'])) {
+                    echo "0";
+                    //mostra a quantidade de itens que estão no carrinho de compras
+                } else {
+                    //conta  a quntidade de itens
+                    $contar = count($_SESSION['itens']);
+                    //mostra a quntidade de itens
+                    echo $contar;
+                }
+                ?>
+            </div>
+        </a>
+        </div>
+    </div>
+</header>
+
+<div class="nav-header shadow-i">
+    <nav id="nav" class="col-lg-9 col-md-9 col-sm-9">
+        <!-- botão que aparece no mobile para a exibissão do menu mobile   -->
+        <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+            <!-- icone de hamburger para menu mobile  -->
+            <span id="hamburger"></span>
+        </button>
+        <ul id="menu" role="menu">
+            <!-- redirecionamento das paginas do menu  -->
+            <li><a href="index.php">Home</a></li>
+            <li><a href="pagxbox.php">Xbox</a></li>
+            <li><a href="pagplaystation.php">Playstation</a></li>
+            <li><a class="borda-nintendo" href="pagnintendo.php">Nintendo</a></li>
+        </ul>
+    </nav>
+</div>
+<!-- Fim do cabeçalho do site/menu -->
+<!-- Inicio do SlideShow-->
+<div class="titulo-slide col-lg-12 col-md-12 col-sm-12">
+    <h2> Adicionados Recentemente:</h2>
+</div>
+<!-- div para agrupar o slideshow  -->
+<div class="wrapper">
+    <div class="slide-wrapper" data-slide="wrapper">
+         <!-- botões para movimentar o slide para frete ou para atras -->
+        <button class="slide-nav-button slide-nav-previous fas fa-chevron-left" data-slide="nav-previous-button"></button>
+        <button class="slide-nav-button slide-nav-next fas fa-chevron-right" data-slide="nav-next-button"></button>
+        <div class="slide-list" data-slide="list">
+            <div class="slide-item" data-slide="item" data-index="0">
+                <div class="slide-content">
+                    <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                    <a href="infoprodutos.php?<?php echo "id_produto=24" ?>">
+                        <img class="slide-image" src="slideimg/elden-ring.jpg" alt="Elden Ring" /></a>
+                    <div class="slide-description">
+                         <!-- mostra o nome do produto e a sua categoria -->
+                        <h3>Elden Ring</h3>
+                        <p>Open World - Rpg - Ação - Aventura (Ps4/Ps5 e Xbox One)</p>
+                    </div>
+                </div>
+            </div>
+            <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+            <a href="infoprodutos.php?<?php echo "id_produto=23" ?>">
+                <div class="slide-item" data-slide="item" data-index="1">
+            </a>
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=23" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/harry-potter.jpg" alt="Hogwarts Legacy" /></a>
+                <div class="slide-description">
+                     <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>Hogwarts Legacy</h3>
+                    <p>Single Player - Rpg - Open World - Ação - Aventura (Ps4/Ps5, Xbox One e Nintendo Switch) </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide-item" data-slide="item" data-index="2">
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=22" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/god-of-war.jpg" alt="God of War: Ragnarok" /></a>
+                <div class="slide-description">
+                     <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>God of War Ragnarok</h3>
+                    <p>Single Player - Ação - Aventura - Rpg (Ps4/Ps5)</p>
+                </div>
+            </div>
+        </div>
+        <div class="slide-item" data-slide="item" data-index="3">
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=18" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/horizon.jpeg" alt="Horizon Forbidden West" /></a>
+                <div class="slide-description">
+                     <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>Horizon Forbidden West</h3>
+                    <p>Single Player - Rpg - Luta - Aventura - Ação (Ps4/Ps5) </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide-item" data-slide="item" data-index="4">
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=7" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/Pokemon.jpg" alt="pokemon" /></a>
+                <div class="slide-description">
+                     <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>Pokemon Scarlet & Violet</h3>
+                    <p>Rpg - Open World - Aventura (Nintendo Switch) </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide-item" data-slide="item" data-index="5">
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=32" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/one-piece.jpg" alt="One piece Odyssey" /></a>
+                <div class="slide-description">
+                     <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>One Piece Odyssey</h3>
+                    <p>Single Player - Rpg - Aventura - Ação (Ps4/Ps5 e Xbox One) </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide-item" data-slide="item" data-index="6">
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=29" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/Zelda.jpg" alt="Zelda: Tears of the kingdom" /></a>
+                <div class="slide-description">
+                     <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>The legend of Zelda: Tears of the Kingdom</h3>
+                    <p>Single Player - Open World - Ação - Aventura (Nintendo Switch) </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide-item" data-slide="item" data-index="7">
+            <div class="slide-content">
+                <!-- redireciona o usuario para a pagina quando ele selecionar o produto  -->
+                <a href="infoprodutos.php?<?php echo "id_produto=31" ?>">
+                <!-- mostra a imagem do produto -->
+                    <img class="slide-image" src="slideimg/cod.jpg" alt="Mordern Warfare 2" /></a>
+                <div class="slide-description">
+                    <!-- mostra o nome do produto e a sua categoria -->
+                    <h3>Call of Duty: Mordern Warfare 2</h3>
+                    <p>Fps - Ação - Aventura (Ps4/Ps5 e Xbox One)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slide-controls" data-slide="controls-wrapper">
+    </div>
+</div>
+</div>
+<!-- Fim de SlideShow -->
+<div class="produto_vendido">
+    <?php
+    include('../php/conexao.php');
+    $selectt = mysqli_query($mysqli, "SELECT * FROM produtos ORDER BY num_vendas DESC");
+    $fetch = mysqli_fetch_assoc($selectt);
+    ?>
+    <h1>Nosso Produto Mais Vendido</h1>
+    <!-- redireciona o usuario até a pagina de informação do produto -->
+    <a href="infoprodutos.php?<?php echo 'id_produto=';
+                                echo $fetch["id_produto"]; ?>">
+        <div class="divprincipal col-sm-12 col-md-3 col-lg-2">
+            <div class="jogopesquisa">
+                <div class="imgoverflow">
+                    <div class="imgzoom">
+                        <!-- mostra a imagem de cada produto -->
+                        <img class="img-produtos" src="<?php echo $fetch['caminho']; ?>"><br>
+                    </div>
+                </div>
+                <?php echo $fetch['nome']; //mostra o nome de cada produto 
+                ?><br>
+                <?php echo "R$";
+                echo number_format($fetch['valor'], 2, ",", "."); //mostra o valor de cada produto
+                ?><br>
+            </div>
+        </div>
+    </a>
+</div>
+<!-- Inicio do rodape do Site -->
+<footer>
+    <section class="footer col-lg-12 col-md-12 col-sm-12">
+        <nav class="div-img-scs col-sm-12 col-md-3 col-lg-3">
+            <img class="img-scs" src="icons/saocaetano.png" alt="">
+        </nav>
+        <ul class="list col-lg-6 col-md-6 col-sm-12">
+            <li class="listra">
+                <a href="index.php">Home</a>
+            </li>
+            <li class="listra">
+                <a href="pagxbox.php">Xbox</a>
+            </li>
+            <li class="listra">
+                <a href="pagplaystation.php">Playstation</a>
+            </li>
+            <li>
+                <a href="pagnintendo.php">Nintendo</a>
+            </li>
+        </ul>
+
+        <div class="contatos col-lg-3 col-md-3 col-sm-12">
+            <p> Contatos: (11)98454-0148 ou através do nosso e-mail G4m3sForYou@gmail.com
+                <br> <br>
+
+        </div>
+
+    </section>
+</footer>
+<div class="copyright col-sm-12 col-lg-12 col-md-12">
+    <span class="material-symbols-outlined copy">
+        copyright
+    </span>
+    <p> Atenção é importante informar que esse site é apenas para fins educacionais e sem fins lucrativos</p>
+    </p>
+</div>
+<!--Fim do Rodape do Site-->
+<!-- links para o js  -->
+<script src="../js/slide.js"></script>
+<script src="../js/menu.js"></script>
+<script>
+
+function carregar(){
+//animação do carregamento do icone de usuario
+//desabilita a animação de carregamento quando o icone terminar de carregar
+document.getElementsByClassName("carregamento")[0].style.display = "none";
+//quando o icone carregar ele sera exibido
+document.getElementsByClassName("carregado")[0].style.display = "flex";
+
+//animação do carregamento do icone do carrinho de compras
+//desabilita a animação de carregamento quando o icone terminar de carregar
+document.getElementsByClassName("carregamento1")[0].style.display = "none";
+//quando o icone carregar ele sera exibido
+document.getElementsByClassName("carregado1")[0].style.display = "block";
+}
+    initSlider({
+        //caso seja falso o slide se torna apenas manual
+        autoPlay: true,
+        //começa pelo primeiro item do slide
+        startAtIndex: 0,
+        //tempo de mudança das imagens do slide
+        timeInterval: 3500
+    })
+</script>
+</body>
+</html>
